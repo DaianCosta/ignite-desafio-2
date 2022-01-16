9c067c154bbd438a62a00d52a3fb2ed9b7a028fc
DaianCosta <desenvolvimentodaian@gmail.com>
2022-01-16 00:04:32 -0300

    add

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { GenreResponseProps } from "../models/CategoriesModel";
+import { api } from "../services/api";
+import "../styles/sidebar.scss";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    // Complete aqui
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
